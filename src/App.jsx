d5ed2345a7fc987c455bb34bf5a412e695bbddd4
Yevhen Khreptun <khreptun.e@gmail.com>
2023-12-02 22:23:00 -0500

    auto sorting for tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <h1 className="mx-auto p-4 text-center text-3xl font-bold text-red-300">
+        Hello
+      </h1>
+    </>
+  );
 }
 
 export default App;
