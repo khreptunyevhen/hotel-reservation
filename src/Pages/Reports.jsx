d0ce3c9a6107bb859946ec9995d773c4e7653b7f
Yevhen Khreptun <khreptun.e@gmail.com>
2023-12-09 14:20:09 -0500

    download a report via a pdf file

--- src/Pages/Reports.jsx
+++ src/Pages/Reports.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react"
+import { jsPDF } from "jspdf"
+import autoTable from 'jspdf-autotable'
+import { useReservationData } from "../context/reservationDataContext"
+
+const importantColumns = [
+  "owner", "unit", "price", "owner email"
+]
+
 function Reports() {
+  const [error, setError] = useState(null)
+  const { reservationData } = useReservationData()
+
+  const accessibleReservationRoom = reservationData?.filter(reservation => reservation.status !== "canceled")
+
+  function generatePDF() {
+    const report = new jsPDF()
+
+    report.text(`Hello ${"main"}!`, 20, 20)
+
+    report.autoTable({
+      head: [importantColumns.map(header => header)],
+      body: accessibleReservationRoom.map(row =>
+        importantColumns.map(col => row[col])
+      ),
+      startY: 40,
+    })
+
+    if (reservationData) {
+      report.save(`${"main"}-report.pdf`)
+      setError(null)
+    } else {
+      setError("Check your an excel file!")
+    }
+  }
+
   return (
-    <div>Reports</div>
+    <section>
+      <h2>Reports</h2>
+      <button onClick={generatePDF} type="button">Download report</button>
+      {
+        error ? <p>{error}</p> : null
+      }
+    </section>
   )
 }
 
