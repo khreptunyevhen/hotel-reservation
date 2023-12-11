5088a4423fd34a90f55fabeefd1c05823300c4f0
Yevhen Khreptun <khreptun.e@gmail.com>
2023-12-10 22:38:45 -0500

    logo as a link

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,10 +1,13 @@
-import { AlignRight, X } from 'lucide-react';
+import { Link } from "react-router-dom"
+import { AlignRight, X } from 'lucide-react'
 
 function Header({ isOpenMenu, onHandleOpenMenu }) {
   return (
     <header className="py-8 max-h-28 shadow-xl bg-background relative">
       <div className="container px-4 mx-auto flex items-center justify-between">
-        <img src="/images/logo.svg" alt="werfy - we rent for you" />
+        <Link to="/">
+          <img src="/images/logo.svg" alt="werfy - we rent for you" />
+        </Link>
         <button className="z-50 relative" onClick={onHandleOpenMenu}>
           {
             isOpenMenu ? <X className="h-10 w-10 md:hidden" /> : <AlignRight className="h-10 w-10 md:hidden" />
