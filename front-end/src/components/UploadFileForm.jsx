8b0aa97884636c1a0f6021aa4d9dd77274e08926
Yevhen Khreptun <khreptun.e@gmail.com>
2023-12-11 21:44:27 -0500

    responsive form for uploading files

--- front-end/src/components/UploadFileForm.jsx
+++ front-end/src/components/UploadFileForm.jsx
@@ -6,7 +6,7 @@ function UploadFileForm() {
   const { handleFile, handleSubmitFile, error } = useReservationData()
 
   return (
-    <form className="flex items-center justify-between bg-background rounded-lg p-4 mb-4" onSubmit={handleSubmitFile}>
+    <form className="flex flex-col items-start space-y-4 bg-background rounded-lg p-4 mb-4 md:flex-row md:items-center md:justify-between md:space-y-0" onSubmit={handleSubmitFile}>
       <input name="file" className="hidden" id="file" type="file" required onChange={handleFile} />
       <label htmlFor="file" className="flex items-center gap-2 cursor-pointer px-4 py-2 border border-primary rounded-lg font-medium hover:bg-primary transition duration-300">
         <UploadCloud />
